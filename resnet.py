ac5058ad53c97d82c8e8cbf9d1da61ed0e1180a4
Sharanda <s2630575@vuw.leidenuniv.nl>
2023-05-13 17:51:17 +0200

    train copy

--- resnet.py
+++ resnet.py
@@ -7,9 +7,11 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, GlobalAveragePooling2D
+from tensorflow.keras.layers import Dense, Flatten, GlobalAveragePooling2D, Dropout, AveragePooling2D
 from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.keras.models import Model
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+
+data = pd.read_csv("/content/thesis/labels/train_labels.csv")
---
+++ trainingcopy.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+# Set the directory paths
+folder1 = '../thesis_train/train_AID'
+folder2 = '../thesis_train2/train_AID2'
+output_folder = '../train_all'
+
+# Loop through the files in the first folder
+for filename in os.listdir(folder1):
+    # Copy the file to the output folder
+    shutil.copy(folder1 + "/" + filename, output_folder + "/" + filename)
+
+# Loop through the files in the second folder
+for filename in os.listdir(folder2):
+    # Copy the file to the output folder
+    shutil.copy(folder2 + "/" + filename, output_folder +"/" + filename)
