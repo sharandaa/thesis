eb0ee4b6a34fa6469ba67363e14ccc5501386230
Sharanda <s2630575@vuw.leidenuniv.nl>
2023-05-18 12:28:51 +0200

    resnet adjust

--- resnet.py
+++ resnet.py
@@ -84,7 +84,7 @@ model = Model(inputs=base_model.input, outputs=predictions)
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Set up callbacks to save the best model weights and stop training early if validation loss stops improving
-checkpoint = ModelCheckpoint('best_resnet.h5', save_best_only=True, save_weights_only=True, monitor='val_loss', mode='min', verbose=1)
+checkpoint = ModelCheckpoint('best_resnetmodel.h5', save_best_only=True, save_weights_only=False, monitor='val_loss', mode='min', verbose=1)
 earlystop = EarlyStopping(monitor='val_loss', mode='min', patience=10, verbose=1)
 
 history = model.fit_generator(
