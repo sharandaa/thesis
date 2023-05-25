82783adcd61b73de84cd92057d5ff207b3f0605f
Sharanda <s2630575@vuw.leidenuniv.nl>
2023-05-25 23:27:20 +0200

    tfds test

--- testimport.py
+++ testimport.py
@@ -1,4 +1,4 @@
 #import tensorflow as tf
 import tensorflow_datasets as tfds
-ds = tfds.load("tftest_AID", data_dir='/scratch/s2630575/tfds')
+ds = tfds.load("AIDtestx2", data_dir='/scratch/s2630575/tfds')
 print(ds)
