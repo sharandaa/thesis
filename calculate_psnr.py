1a77f466aeb3a450abb1762e2f09d388b7a7d8c1
Sharanda <s2630575@vuw.leidenuniv.nl>
2023-06-27 14:47:30 +0200

    espcn finetuned

--- calculate_psnr.py
+++ calculate_psnr.py
@@ -5,6 +5,7 @@ import os
 from skimage.metrics import structural_similarity as ssim
 from natsort import natsorted
 import matplotlib.pyplot as plt
+import random
 
 n_image = 1000
 og_dir = 'test_AID'
@@ -18,6 +19,12 @@ ssim_dict = {}
 
 #upsampled_dirs = ['/home/s2630575/thesis/espcnmodel/ESPCN_x3', '/home/s2630575/SwinIR/results/swinir_lightweight_sr_x3']
 #names = ['espcn', 'lightweight swinir x2']
+
+upsampled_dirs = ['/home/s2630575/thesis/espcnmodel/ESPCN_x3', 
+                  '/home/s2630575/SwinIR/results/swinir_classical_sr_x3', 
+                  '/home/s2630575/SwinIR/results/swinir_lightweight_sr_x3', ]
+
+names = ['ESPCN', 'SwinIR Classical', 'SwinIR Lightweight']
   
 def main():
     for i in range(5):
@@ -105,6 +112,121 @@ def main():
 
     plt.savefig("ssimplotx2.pdf", format="pdf", bbox_inches="tight")
 
+def x3plot():
+    finetuned_dirs = ['d', 'd', 'd']
+    psnr_list_ft = []
+    ssim_list_ft = []
+
+    for i in range(3):
+        finetuned_dir = finetuned_dirs[i] 
+        print(finetuned_dir)
+    
+        # Get list of files in each folder
+        og_files = os.listdir(og_dir)
+        upsampled_files = os.listdir(finetuned_dir)
+
+        # Sort the files alphabetically to match the pairs
+        og_files = natsorted(og_files)
+        upsampled_files = natsorted(upsampled_files)
+
+        # Loop over the files in the folders and read the image pairs
+        for file1, file2 in zip(og_files, upsampled_files):
+            og_img = cv2.imread(os.path.join(og_dir, file1))
+            up_img = cv2.imread(os.path.join(finetuned_dir, file2))
+            
+            #print(file1, file2)
+            score = cv2.PSNR(og_img, up_img)
+            #print(score)
+            psnr_list_ft.append(score)
+
+            # Convert the images to grayscale
+            gray1 = cv2.cvtColor(og_img, cv2.COLOR_BGR2GRAY)
+            gray2 = cv2.cvtColor(up_img, cv2.COLOR_BGR2GRAY)
+            ssim_value = ssim(gray1, gray2)
+            ssim_list_ft.append(ssim_value)
+
+    # Set the random seed for reproducibility
+    random.seed(42)
+
+    # Randomly select 1000 values from the original list
+    psnr_finetuned = random.sample(psnr_list_ft, 1000)
+    ssim_finetuned = random.sample(ssim_list_ft, 1000)
+
+    for i in range(3):
+        upsampled_dir = upsampled_dirs[i] 
+        print(upsampled_dir)
+        psnr_list = []
+        ssim_list = []
+
+        # Get list of files in each folder
+        og_files = os.listdir(og_dir)
+        upsampled_files = os.listdir(upsampled_dir)
+
+        # Sort the files alphabetically to match the pairs
+        og_files = natsorted(og_files)
+        upsampled_files = natsorted(upsampled_files)
+
+        # Loop over the files in the folders and read the image pairs
+        for file1, file2 in zip(og_files, upsampled_files):
+            og_img = cv2.imread(os.path.join(og_dir, file1))
+            up_img = cv2.imread(os.path.join(upsampled_dir, file2))
+            
+            #print(file1, file2)
+            score = cv2.PSNR(og_img, up_img)
+            #print(score)
+            psnr_list.append(score)
+            #print(file1, file2)
+            #total_psnr += score
+
+            # Convert the images to grayscale
+            gray1 = cv2.cvtColor(og_img, cv2.COLOR_BGR2GRAY)
+            gray2 = cv2.cvtColor(up_img, cv2.COLOR_BGR2GRAY)
+            ssim_value = ssim(gray1, gray2)
+            ssim_list.append(ssim_value)
+            #total_ssim += ssim_value
+
+            #total += 1
+            # Do something with the image pairs
+            # For example, display them side by side
+            #cv2.imshow('Image Pair', cv2.hconcat([og_img, up_img]))
+
+        psnr_dict[names[i]] = psnr_list
+        ssim_dict[names[i]] = ssim_list
+
+    psnr_dict['Fine-tuned ESPCN'] = psnr_finetuned
+    ssim_dict['Fine-tuned ESPCN'] = ssim_finetuned
+     
+    group_data = [values for key, values in psnr_dict.items()]
+    group_data_ssim = [values for key, values in ssim_dict.items()]
+
+    #print(group_data)
+
+    # psnr
+    fig, ax = plt.subplots()
+    ax.boxplot(group_data)
+
+    # Add labels and title
+    ax.set_xticklabels(psnr_dict.keys())
+    #ax.set_xlabel('Groups')
+    ax.set_ylabel('PSNR (dB)')
+    ax.set_title('PSNR scores of scale x3')
+    plt.xticks(rotation = 45, fontsize=15)
+
+    plt.savefig("psnrplotx3.pdf", format="pdf", bbox_inches="tight")
+
+    # ssim
+    fig, ax = plt.subplots()
+    ax.boxplot(group_data_ssim)
+
+    # Add labels and title
+    ax.set_xticklabels(ssim_dict.keys())
+    #ax.set_xlabel('Groups')
+    ax.set_ylabel('SSIM')
+    ax.set_title('SSIM scores of scale x3')
+    plt.xticks(rotation = 45, fontsize=15)
+
+    plt.savefig("ssimplotx3.pdf", format="pdf", bbox_inches="tight")
+
 def single_image():
     og_img = cv2.imread('C:/Users/shara/OneDrive/Documents/Scriptie/AID/mod_AID/all_bicubic_2x_300/airport_24x2.jpg')
     up_img = cv2.imread('C:/Users/shara/OneDrive/Documents/Scriptie/AID/mod_AID/Airport/airport_24.jpg')
@@ -125,6 +247,7 @@ def size():
 
 
 if __name__ == "__main__":
-    main()
+    #main()
     #single_image()
     #size()
+    x3plot()
